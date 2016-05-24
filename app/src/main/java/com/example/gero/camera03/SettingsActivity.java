4a293c14d615103a2ee46e9f2487af9df5aa1a37
Ladislav Kluka <lkluka@gmail.com>
2016-05-24 23:25:06 +0200

    initial commit

---
+++ app/src/main/java/com/example/gero/camera03/SettingsActivity.java
@@ -0,0 +1,17 @@
+package com.example.gero.camera03;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+/**
+ * Created by gero on 5/24/16.
+ */
+public class SettingsActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.settings_layout);
+
+    }
+}
